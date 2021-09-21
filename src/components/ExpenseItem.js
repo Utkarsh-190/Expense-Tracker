3bd588adf232a08b08cdbaeaee73b65a9773a6ef
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-09-21 21:21:45 +0530

    styles added to expenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+    const expenseDate = new Date();
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 2000;
+
+    return (
+        <div className = "expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className = "expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className = "expense-item__price">{expenseAmount} Rs</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
