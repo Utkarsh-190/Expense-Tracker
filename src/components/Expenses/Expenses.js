f16ff58b34281a0bf4af9830d7677a67bfc74cbf
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-09-23 23:00:06 +0530

    make list of expenses dynamic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,35 +11,29 @@ function Expenses(props) {
     setFilterData(enteredFilteData);
   };
 
+  const filteredExpenses = props.expenses;
+
+  let expensesContent = <p>No expense available for this year</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filterData}
         onChangeFilter={onChangeFilterHandler}
       />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
+      {expensesContent}
     </Card>
   );
 }
