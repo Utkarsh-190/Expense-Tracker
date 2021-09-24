bc9f9fcc6bcb871e5a095315a3991c314a03abd3
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-09-24 23:21:47 +0530

    cancelation feature added in new expense form

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   const onSaveHandler = (expense) => {
     props.onAddExpense(expense);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={onSaveHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 }
